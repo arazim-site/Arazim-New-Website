6ef1688185dd20eab3bd0033bd0e15df58b8d793
moolulu <brandt.shmuel@gmail.com>
2026-05-19 11:35:35 +0300

    changed people page navigation name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const sitePages = [
   { title: "דף הבית", path: "/" },
   { title: "המסלול", path: "/track" },
-  { title: "האנשים", path: "/people" },
+  { title: "הבוגרים", path: "/people" },
   { title: "המיונים", path: "/selection" },
   { title: "שאלות ותשובות", path: "/faq" },
 ];
