55c4772f86289b25b655d1289d10bdaa292254ef
moolulu <brandt.shmuel@gmail.com>
2026-05-19 11:32:29 +0300

    deleted unnecessary filed and fixed phrasing

--- src/config.js
+++ src/config.js
@@ -3,6 +3,5 @@ export const sitePages = [
   { title: "המסלול", path: "/track" },
   { title: "האנשים", path: "/people" },
   { title: "המיונים", path: "/selection" },
-  { title: "אתגרים", path: "/challenges" },
   { title: "שאלות ותשובות", path: "/faq" },
 ];
