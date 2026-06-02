2e37f561aa77e43834ce0219024a3f017bdbb3ee
arazim-site <45860017+arazim-site@users.noreply.github.com>
2026-06-02 12:33:40 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://moolulu.github.io',
+  site: 'https://araz.im',
   base: 'Arazim-Website',
   prefetch: {
     prefetchAll: true,
